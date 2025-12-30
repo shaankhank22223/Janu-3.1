274c7f6d49bf6b91796a07a462850e247bd0b8b9
Mukku Rajput <rajputmukku02@gmail.com>
2025-12-30 13:22:30 +0000

    Edit uid.js

--- models/commands/0_ARIF-UID.js
+++ models/commands/0_ARIF-UID.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "uid",
 	version: "1.0.0",
 	hasPermssion: 0,
-	credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
+	credits: "ARIF BABU",
 	description: "Get User ID.",
 	commandCategory: "Tools",
 	cooldowns: 5
