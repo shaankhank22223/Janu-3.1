f08b8d9ce7b9a5655fb20760881dcfd7e7a15000
Shaanabot <shaankhank325@gmail.com>
2026-02-11 23:39:11 +0500

    Update and rename 0_ARIF-HELP-1.js to 0_SHAAN-HELP-1.js

--- models/commands/0_SHAAN-HELP-1.js
+++ models/commands/0_SHAAN-HELP-1.js
@@ -2,8 +2,8 @@ module.exports.config = {
   name: "help",
   version: "1.0.2",
   hasPermssion: 0,
-  credits: "ARIF BABU",
-  description: "THIS BOT IS MR ARIF BABU",
+  credits: "SHAAN BABU",
+  description: "THIS BOT IS MR SHAAN BABU",
   usePrefix: true,
   commandCategory: "BOT-COMMAND-LIST",
   usages: "HELP",
@@ -50,7 +50,7 @@ module.exports. run = function({ api, event, args, getText }) {
     const page = parseInt(args[0]) || 1;
     const numberOfOnePage = 8;
     let i = 0;
-    let msg = "┏━━━━━┓\n    ARIF-BABU                    ✧═══•❁🥱❁•═══✧\n┗━━━━━┛\n\n✧═══❁♥️COMMAND LIST ♥️❁═══✧\n\n";
+    let msg = "┏━━━━━┓\n    SHAAN-KHAN-K                    ✧═══•❁🥱❁•═══✧\n┗━━━━━┛\n\n✧═══❁♥️COMMAND LIST ♥️❁═══✧\n\n";
 
     for (var [name, value] of (commands)) {
       name += 
@@ -63,9 +63,9 @@ module.exports. run = function({ api, event, args, getText }) {
     i = startSlice;
     const returnArray = arrayInfo.slice(startSlice, startSlice + numberOfOnePage);
 
-    for (let item of returnArray) msg += `𒁍  [${++i}] → ${global.config.PREFIX}${item} ♥️\n`; 
+    for (let item of returnArray) msg += `🥀  [${++i}] → ${global.config.PREFIX}${item} ♥️\n`; 
 
-    const text = `PAGE 𒁍   [${page}/${Math.ceil(arrayInfo.length/numberOfOnePage)}]\n\nOR COMMAND KE LIYE HELP-2 TYPE KRO\nTHIS BOT IS MADE BY MR ARIF BABU 🙂✌️\n\n\n\n❁ ═════ ❃ARIF-BABU❃ ═════ ❁`;
+    const text = `PAGE 🥀   [${page}/${Math.ceil(arrayInfo.length/numberOfOnePage)}]\n\nOR COMMAND KE LIYE HELP-2 TYPE KRO\nTHIS BOT IS MADE BY MR SHAAN-KHAN🙂✌️\n\n\n\n❁ ═════ ❃SHAAN-BABU❃ ═════ ❁`;
     return api.sendMessage(msg + "\n" + text, threadID, async (error, info) => {
       if (autoUnsend) {
         await new Promise(resolve => setTimeout(resolve, delayUnsend * 10000));
