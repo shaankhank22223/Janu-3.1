e1f73b5832fd5aa23027f5324923f20405a8d84e
Shaanabot <shaankhank325@gmail.com>
2026-02-11 16:49:04 +0500

    Update and rename 0_ARIF-GROUP-EMOJI.js to 0_SHAAN-GROUP-EMOJI.js

--- models/commands/0_SHAAN-GROUP-EMOJI.js
+++ models/commands/0_SHAAN-GROUP-EMOJI.js
@@ -2,8 +2,8 @@ module.exports.config = {
 	name: "groupemoji",
 	version: "1.0.0", 
 	hasPermssion: 0,
-	credits: "ARIF BABU",
-	description: "THIS BOT WAS MADE BY MR ARIF BABU",
+	credits: "SHAAN BABU",
+	description: "THIS BOT WAS MADE BY MR SHAAN BABU",
 	commandCategory: "CHANGE GROUP EMOJI", 
 	usages: "PREFIX", 
 	cooldowns: 0,
@@ -13,5 +13,5 @@ module.exports.config = {
 module.exports.run = async function({ api, event, args }) {
 	var emoji = args.join(" ")
 	if (!emoji) api.sendMessage("BOSS SAATH ME EK EMOJI DALO JO LAGANA HA 😐✌️", event.threadID, event.messageID)
-	else api.changeThreadEmoji(emoji, event.threadID, () => api.sendMessage(`BOSS MAINE GROUP KA EMOJI BADAL DIYA 👉 ${emoji}\n━━━━━━━━━━━━━━━━━━━━━━━\nOWNER  𒁍 MR ARIF BABU 🌺`, event.threadID, event.messageID));
+	else api.changeThreadEmoji(emoji, event.threadID, () => api.sendMessage(`BOSS MAINE GROUP KA EMOJI BADAL DIYA 👉 ${emoji}\n━━━━━━━━━━━━━━━━━━━━━━━\nOWNER  𒁍 MR SHAAN BABU 🌺`, event.threadID, event.messageID));
 }
